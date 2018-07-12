de00344777eed31b3691420ec00c1d85083882c9
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-12 22:53:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,13 +5,14 @@ var urlsToCache = [
   '/assets/bootstrap/js/bootstrap.min.js'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('register', function(event) {
   // Perform install steps
-  event.waitUntil(
+          console.log('Opened cache');
+  /*event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
-  );
+  );*/
 });
