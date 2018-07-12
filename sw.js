c461754c2abfd2572d8fc1859647861e84c65181
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-12 23:01:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-var CACHE_NAME = 'shalom-cache-v1';
-var urlsToCache = [
-  '/',
-  '/assets/bootstrap/css/bootstrap.min.css',
-  '/assets/bootstrap/js/bootstrap.min.js'
-];
-
-self.addEventListener('register', function(event) {
-  // Perform install steps
-          console.log('Opened cache');
-  /*event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );*/
-});
+self.addEventListener('message', function(e) {
+  self.postMessage(e.data);
+}, false);
