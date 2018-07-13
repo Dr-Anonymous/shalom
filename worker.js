85ea45318cb60f6f78250018eafe690bbb1104ba
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-13 23:34:40 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -9,14 +9,15 @@ function timedCount() {
 timedCount();
 */
 function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback= ", true);
-    xmlhttp.send();
- xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
                postMessage(xmlhttp.responseText["result1"]);
         }
     };
 
 }
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback= ", true);
+    xmlhttp.send();
+ 
 loadXMLDoc();
