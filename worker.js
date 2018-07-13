0eca4d2192608e49f4fdf4d0265119bf09953067
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-13 23:06:24 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ function loadXMLDoc() {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
            if (xmlhttp.status == 200) {
-               postMessage(e.result1);
+               postMessage(xmlhttp.responseText.result1);
            }
            else if (xmlhttp.status == 400) {
               postMessage('There was an error 400');
