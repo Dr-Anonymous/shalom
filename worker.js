17d2a3bb5770ef144f2b5331e45b2e43862e07c7
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-13 23:24:15 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -10,21 +10,6 @@ timedCount();
 */
 function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-               xmlhttp.responseText;
-           }
-           else if (xmlhttp.status == 400) {
-              postMessage('There was an error 400');
-           }
-           else {
-               postMessage('something else other than 200 was returned');
-           }
-        }
-    };
-
     xmlhttp.open("GET", "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec", true);
     xmlhttp.send();
 }
