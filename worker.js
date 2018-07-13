084be9ea995ec68c25d830fa8a545e6df14b3f0c
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-13 22:08:26 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,22 @@
-var i = 0;
+/*var i = 0;
 
 function timedCount() {
     i = i + 1;
     postMessage(i);
-    setTimeout("timedCount()",100);
+    setTimeout("timedCount()",500);
 }
 
 timedCount();
+*/
+var url = "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback=loadData";
+// Make an AJAX call to Google Script
+var request = jQuery.ajax({
+      crossDomain: true,
+      url: url ,
+      method: "GET",
+      dataType: "jsonp"
+    });
+  }
+  // print the returned data from jsonp
+  function loadData(e) {
+     postMessage(e.result1);}
