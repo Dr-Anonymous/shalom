69d90db57795388ebd9a667e58d26471d89bc188
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-15 23:06:53 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,3 @@
-/*var i = 0;
-
-function timedCount() {
-    i = i + 1;
-    postMessage(i);
-    setTimeout("timedCount()",500);
-}
-
-timedCount();
-*/
-
 var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
 req.open('GET', "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback= ", true);
