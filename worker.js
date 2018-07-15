1db0b67877d8f194e60154d36af825688f3bf9de
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-15 23:02:12 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -13,7 +13,7 @@ var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
 req.open('GET', "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback= ", true);
 req.onload  = function() {
-   var jsonResponse = JSON.parse(req.responseText.slice(0,-1));
+   var jsonResponse = JSON.parse(req.responseText.slice(1,-1));
    // do something with jsonResponse
     postMessage(jsonResponse);
 };
