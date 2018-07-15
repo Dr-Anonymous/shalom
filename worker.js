5f4a05a0a948ea1a8c9ebbd20a0cdb80d14628fb
Dr-Anonymous <gangrenesoul@gmail.com>
2018-07-15 23:35:07 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,9 @@
+var url = '';
+self.addEventListener('message', function(event){
+    url= event.data;
+   console.log("SW Received Message: " + url);
+});
+
 var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
 req.open('GET', "https://script.google.com/macros/s/AKfycbwX-bdUXwFHyej-VLVdxQc9v5izgvXJUKiKcWWHVYhnlp1B2Np9/exec?callback= ", true);
